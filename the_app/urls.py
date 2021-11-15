db83257f54aff737273fe4de5270acc17eb0ffed
James Chandler <stillsound@protonmail.com>
2021-11-15 15:35:07 -0800

    settings.DEGUG ==fales in urls.py in the_app

--- the_app/urls.py
+++ the_app/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('eseven.urls')),
 ]
-if settings.DEBUG:
+if settings.DEBUG == False:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
