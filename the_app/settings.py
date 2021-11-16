9703b5d52ebc547e03d5eb86ba2df1e287445ff5
James Chandler <stillsound@protonmail.com>
2021-11-15 18:50:38 -0800

    staticfiles

--- the_app/settings.py
+++ the_app/settings.py
@@ -159,13 +159,10 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    'staticfiles'
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-
-
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
