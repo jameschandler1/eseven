14b644e3aa13aa2e1a39c694897603da57c49c4f
James Chandler <stillsound@protonmail.com>
2021-11-17 21:33:31 -0800

    all

--- payments/urls.py
+++ payments/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('payments/', PaymentView.as_view(), name='payments'),
     path('config/', stripe_config, name='config'),
     path('create-checkout-session/', create_checkout_session, name='create-checkout-session'),
-    path('success/', SuccessView.as_view(), name='success'), 
+    path('success', SuccessView.as_view(), name='success'), 
     path('cancelled/', CancelledView.as_view(), name='cancelled'), 
     path('webhook/', stripe_webhook, name='webhook'), 
 ]
