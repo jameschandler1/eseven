74e01cc03fee6741d61c58f0abf9af830ab0723a
James Chandler <stillsound@protonmail.com>
2021-11-20 13:32:58 -0800

    adjust settings for heroku before deployment

--- eseven/urls.py
+++ eseven/urls.py
@@ -7,7 +7,7 @@ from payments.views import *
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('products', ProductListView.as_view(), name='products'),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name='products_details'),
+    path('products/<int:pk>', ProductDetailView.as_view(), name='products_details'),
     path('contact/', ContactView.as_view(), name='contact'),
     path('contact/', contact, name='contact_form'),
     path('cart', CartView.as_view(), name='cart'),
