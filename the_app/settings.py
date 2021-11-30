61b9a2b894cf8b544dd36712498fd22abb667b50
James Chandler <stillsound@protonmail.com>
2021-11-29 16:24:41 -0800

    fix settings before deployment

--- the_app/settings.py
+++ the_app/settings.py
@@ -93,8 +93,8 @@ DATABASES = {
     }
 }
 
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-# django_heroku.settings(locals())
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+django_heroku.settings(locals())
 
 
 # Password validation
